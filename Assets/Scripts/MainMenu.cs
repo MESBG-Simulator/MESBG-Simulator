1773a326c037534294eb9ed7c956ec0a950d84b2
Kevin Denis <kevin.denis.92@gmail.com>
2022-12-28 11:14:29 +0100

    Start unity project Create menu scene with main menu, settings, and quit Create game scene and import 3D models and unity terrain assets

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:21c539b3d19955576616ce55416a7d553cb8b1bd531025bea495dfc045d8f450
+size 411
---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7eef3303b7e45bb016c835e2de8e8e33bba297999da37b52ca89fb795d34e38
+size 1817
