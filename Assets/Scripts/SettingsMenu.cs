ac5c5091d90f68f44071415cc115e82db5523546
Louis van der Elst <louis_vanderelst@vdelst.eu>
2022-12-28 21:32:52 +0100

    Start unity project Create menu scene with main menu, settings, and quit Create game scene and import 3D models and unity terrain assets
    Create terrain, in-game pause menu
    
    Create terrain and pause menu, ignore imported assets

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    // public Slider slider;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        // slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+    }
+
+    public void SetVolume (float volume)
+    {
+        // float volume = slider.value;
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        // PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
