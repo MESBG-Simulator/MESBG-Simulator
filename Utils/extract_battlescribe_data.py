62c8636536fcf302620fa06687784319a5e59be2
Kevin Denis <kevin.denis.92@gmail.com>
2023-01-02 18:32:08 +0100

    BS_db: WIP

--- Utils/extract_battlescribe_data.py
+++ Utils/extract_battlescribe_data.py
@@ -3,6 +3,7 @@ import shutil
 import json
 import json_fix
 from bs4 import BeautifulSoup
+from pathlib import Path
 
 # notes:
 # get roster name from catalogue node, name attribute
@@ -16,51 +17,96 @@ from bs4 import BeautifulSoup
 #   check masterRoster for hero with same name
 #   if name is found, add the hero
 
-
-SOURCE_DIR = os.path.join('C:\\Users', os.environ.get('USERNAME'),
-                          'BattleScribe\\data\\Middle-Earth Strategy Battle Game')
-TARGET_DIR = os.path.join(os.getcwd(), 'Data', 'BattleScribe')
-JSON_PATH = os.path.join('Data', 'battlescribe_data.json')
-
-
-class Model():
-    def __init__(self, name='', movement=0, fight=0, shoot=0, strength=0, defense=0, attack_fight=0, wounds=0, courage=0, keywords='') -> None:
-        self.name = name
-        self.movement = movement
-        self.fight = fight
-        self.shoot = shoot
-        self.strength = strength
-        self.defense = defense
-        self.attack = attack_fight
-        self.wounds = wounds
-        self.courage = 0
-        self.keywords = ''
+ROOT_DIR = Path('..').absolute()
+SOURCE_DIR = Path(Path.home(), 'BattleScribe\\data\\Middle-Earth Strategy Battle Game')
+TARGET_DIR = Path(ROOT_DIR, 'Data', 'BattleScribe')
+JSON_PATH = Path(ROOT_DIR, 'Data', 'battlescribe_data.json')
 
 
-class Hero(Model):
-    def __init__(self, name='', movement=0, fight=0, shoot=0, strength=0, defense=0, attack=0, wounds=0, courage=0, might=0, fate=0, will=0):
-        self.name = name
-        self.movement = movement
-        self.fight = fight
-        self.shoot = shoot
-        self.strength = strength
-        self.defense = defense
-        self.attack = attack
-        self.wounds = wounds
-        self.courage = courage
-        self.might = might
-        self.will = will
-        self.fate = fate
+class Warrior():
+    def __init__(self) -> None:
+        self.name = ''
+        self.movement = -1
+        self.fight = -1
+        self.shoot = -1
+        self.strength = -1
+        self.defense = -1
+        self.attack = -1
+        self.wounds = -1
+        self.courage = -1
         self.keywords = ''
 
     def __json__(self):
         return self.__dict__
 
 
+class Hero(Warrior):
+    def __init__(self):
+        self.name = ''
+        self.movement = -1
+        self.fight = -1
+        self.shoot = -1
+        self.strength = -1
+        self.defense = -1
+        self.attack = -1
+        self.wounds = -1
+        self.courage = -1
+        self.might = -1
+        self.will = -1
+        self.fate = -1
+        self.keywords = ''
+
+
 class RosterCrawler():
     def __init__(self, in_file, master_roster=None):
         self.in_file = in_file
         self.master_roster = master_roster
+        self.warrior_characteristics_list = ['movement', 'fight', 'strength', 'defense',
+                                             'attack', 'wounds', 'courage']
+        self.hero_characteristics_list = ['movement', 'fight', 'strength', 'defense',
+                                          'attack', 'wounds', 'courage', 'might', 'will', 'fate']
+
+    def characteristics_filler(self, model_tag, model=Warrior):
+        if isinstance(model, Hero):
+            characteristics_list = self.hero_characteristics_list
+        else:
+            characteristics_list = self.warrior_characteristics_list
+
+        model.name = model_tag.get('name')
+
+        characteristics_tag = model_tag.find('characteristics')
+        try:
+            for characteristic in characteristics_list:
+                characteristic_tag = characteristics_tag.find(
+                    'characteristic', {'name': characteristic.capitalize()})
+                value = characteristic_tag.contents[0]
+                try:
+                    value = int(value)
+                except:
+                    pass
+
+                if characteristic == 'movement':
+                    try:
+                        value = int(characteristic_tag.contents[0][:-1])
+                    except:
+                        value = -1
+
+                if characteristic == 'fight':
+                    value = characteristic_tag.contents[0].split('/')
+                    if value[0] in ['?', "-"]:
+                        model.fight = -1
+                        model.shoot = -1
+                    else:
+                        model.fight = int(value[0])
+                        if value[1][0] == '-':
+                            model.shoot = -1
+                        else:
+                            model.shoot = int(value[1][0])
+                else:
+                    setattr(model, characteristic, value)
+
+        except IndexError:
+            pass
 
     def read_roster(self):
         with open(self.in_file, encoding='utf-8') as f:
@@ -72,45 +118,37 @@ class RosterCrawler():
             cat_tag = bs_data.find('catalogue')
             roster = Roster(cat_tag.get('name'))
 
+        warrior_tags = bs_data.find_all('profile', {'typeName': 'Warrior'})
+
+        # Here we create a DataBase of profile types and the characteristics they contain
+        profiles_schema_db = {}
+        for profile_type in bs_data.find_all('profileType'):
+            profile_db = {'characteristics': [x['name'] for x in profile_type.find_all('characteristicType')]}
+            profiles_schema_db[profile_type['name']] = profile_db
+
+        profiles_db = {}
+        for profile_name in profiles_schema_db:
+            profile_characteristics = profiles_schema_db[profile_name]['characteristics']
+            profile_db_list = []
+            for profile_tag in bs_data.find_all('profile', {'typeName': profile_name}):
+                profile_db = {'name': profile_tag.get('name')}
+                characteristics_tag = profile_tag.find('characteristics')
+                for profile_characteristic in profile_characteristics:
+                    characteristic_tag = characteristics_tag.find('characteristic', {'name': profile_characteristic})
+                    if len(characteristic_tag.contents):
+                        profile_db[profile_characteristic] = characteristic_tag.contents[0]
+                profile_db_list.append(profile_db)
+            profiles_db[profile_name] = profile_db_list
+
+        for warrior_tag in warrior_tags:
+            w = Warrior()
+            self.characteristics_filler(warrior_tag, w)
+            roster.add_warrior(w)
+
         hero_tags = bs_data.find_all('profile', {'typeName': 'Hero'})
         for hero_tag in hero_tags:
-            h = Hero(name=hero_tag.get('name'))
-            print(roster.name, '->', h.name)
-            characteristics_tag = hero_tag.find('characteristics')
-            characteristics_list = ['movement', 'fight', 'strength', 'defense',
-                                    'attack', 'wounds', 'courage', 'might', 'will', 'fate']
-
-            try:
-                for characteristic in characteristics_list:
-                    characteristic_tag = characteristics_tag.find(
-                        'characteristic', {'name': characteristic.capitalize()})
-                    value = characteristic_tag.contents[0]
-                    try:
-                        value = int(value)
-                    except:
-                        pass
-
-
-                    if characteristic == 'movement':
-                        try:
-                            value = int(characteristic_tag.contents[0][:-1])
-                        except:
-                            value = -1
-
-                    if characteristic == 'fight':
-                        value = characteristic_tag.contents[0].split('/')
-                        if value[0] == '?':
-                            h.fight = -1
-                            h.shoot = -1
-                        else:
-                            h.fight = int(value[0])
-                            h.shoot = int(value[1][0])
-                    else:
-                        setattr(h, characteristic, value)
-
-            except IndexError:
-                pass
-            
+            h = Hero()
+            self.characteristics_filler(hero_tag, h)
             roster.add_hero(h)
 
         if self.master_roster:
@@ -128,10 +166,15 @@ class Roster():
     def __init__(self, name):
         self.name = name
         self.heroes = {}
+        self.warriors = {}
 
     def add_hero(self, hero):
         print(f'Adding {hero.name} to {self.name}')
         self.heroes[hero.name] = hero
+
+    def add_warrior(self, warrior):
+        print(f'Adding {warrior.name} to {self.name}')
+        self.warriors[warrior.name] = warrior
 
     def get_json(self):
         return json.dumps({self.name: self.heroes})
@@ -147,21 +190,21 @@ def list_all_files(file_dir=SOURCE_DIR):
 
 def unzip_all_files(src_dir=SOURCE_DIR, tgt_dir=TARGET_DIR):
     for f_name in os.listdir(src_dir):
-        shutil.unpack_archive(os.path.join(src_dir, f_name), tgt_dir, 'zip')
+        shutil.unpack_archive(Path(src_dir, f_name), tgt_dir, 'zip')
 
 
 def build_rosters():
     rosters = []
-    master_roster_name = 'Middle-Earth_Strategy_Battle_Game.gst'
-    rc = RosterCrawler(os.path.join(TARGET_DIR, master_roster_name))
-    master_roster = rc.read_roster()
-    for f_name in os.listdir(os.path.join(TARGET_DIR)):
-        if f_name != master_roster_name:
-            rc = RosterCrawler(os.path.join(TARGET_DIR, f_name), master_roster)
-            roster = rc.read_roster()
-            rosters.append({'name': roster.name, 'heroes': roster.heroes})
+    master_roster_path = Path(TARGET_DIR, 'Middle-Earth_Strategy_Battle_Game.gst')
+    master_roster_crawler = RosterCrawler(master_roster_path)
+    master_roster = master_roster_crawler.read_roster()
+    for catalogue_path in TARGET_DIR.glob('*.cat'):
+        catalogue_roster_crawler = RosterCrawler(Path(TARGET_DIR, catalogue_path), master_roster)
+        catalogue_roster = catalogue_roster_crawler.read_roster()
+        rosters.append({'name': catalogue_roster.name, 'heroes': catalogue_roster.heroes,
+                       'warriors': catalogue_roster.warriors})
 
-    return json.dumps(rosters, indent=4, sort_keys=True)
+    return json.dumps(rosters, indent=4)
 
 
 if __name__ == '__main__':
